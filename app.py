e93181247e56af1feb12bf7af330f79a5a29567d
Phelix <phelixmbani@gmail.com>
2024-12-18 01:58:38 +0300

    create student

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def initialize_db():
     print('Initializing Database ...!')
 
 def create_teacher():
-    name = str(input('Enter the name of the teacher'))  
+    name = str(input('Enter the name of the teacher: '))  
     email = input('Enter a valid email ')
     teacher = Teacher(name= name, email=email)
     session.add(teacher)
     session.commit()
     
 def update_teacher():
-    id = int(input("Enter the teacher's ID to be updated"))
+    id = int(input("Enter the teacher's ID to be updated: "))
     teacher = session.get(Teacher, id)
     
     if not teacher:
@@ -32,17 +32,27 @@ def update_teacher():
         print(f'Teacher with id {teacher.id} has been updated successfully')
         
 def delete_teacher():
-    id = int(input("Enter the teacher ID to be deleted"))
+    id = int(input("Enter the teacher ID to be deleted: "))
     teacher = session.get(Teacher, id)
     if not teacher:
-        print(f'Teacher with ID {id} does not exist. Enter the correct ID')
+        print(f'Teacher with ID {id} does not exist. Enter the correct ID: ')
         return
     session.delete(teacher)
     session.commit()
+    print(f"Teacher with ID {id} has been deleted successfully: ")
         
-        
-    
 def create_student():
-    name = str(input("Enter the student's name"))
+    first_name = str(input("Enter the student's first name: "))
+    last_name = str(input("Enter the student's lasst name: "))
+    teacher_id = int(input("Enter the teacher ID: "))
+    subject_id = int(input("Enter the student's subject id: "))
+    teacher = session.get(Teacher, teacher_id)
+    subject = session.get(Subject, subject_id)
     
-    
+    if not teacher and subject:
+        print(f"Either the teacher id ({teacher_id} or subject id {subject_id} does not exist)")
+        return
+    student =Student(first_name=first_name, last_name= last_name, teacher_id= teacher_id, subject_id = subject_id)
+    session.add(student)
+    session.commit()
+    print(f'The student with the name {first_name} {last_name} has been created with id {student.id}')
